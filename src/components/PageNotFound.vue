41a7ab4f408d87b1deefdaf3b1997e3989362739
Talha Waleed <talhawaleed460@gmail.com>
2025-02-24 08:43:03 -0800

    add not found page

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,20 @@
+<template>
+    <span>Page not found</span>
+</template>
+<script>
+export default {
+    name: 'PageNotFound'
+}
+</script>
+<style scoped>
+span {
+    color: red;
+    font-size: 20px;
+    font-weight: bold;
+    margin-top: 20px;
+    display: block;
+    text-align: center;
+    
+}
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import {createWebHistory, createRouter} from 'vue-router'
 import HomePageComponent from './components/HomePageComponent.vue'
 import LoginPage from './components/LoginPage.vue';
 import ProfilePage from './components/ProfilePage.vue';
+import PageNotFound from './components/PageNotFound.vue';
 
 const routes = [
     {
@@ -19,6 +20,11 @@ const routes = [
         name: 'ProfilePage',
         path:'/ProfilePage/:name?',
         component: ProfilePage
+    },
+    {
+        name: 'PageNotFound',
+        path:'/:pathMatch(.*)*',
+        component : PageNotFound
     }
 ];
 
